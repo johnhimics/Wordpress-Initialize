676e22feed81cfcd24863b5d2bb3806021c6eca4
John Himics <john@johnhimics.com>
2015-12-31 15:10:28 -0500

    Create pyInitWP.py
    just a layout of what's desired right now.

---
+++ pyInitWP.py
@@ -0,0 +1,32 @@
+# pyInitWP.py
+
+# ** Insert template header
+
+import subprocess
+
+# ** Check if wp-cli is installed
+
+# ** Load the config file
+
+# ** Begin initWP
+
+#BASIC FUNCTIONALITY
+# **** Create users
+# **** update WordPress
+# **** remove twenty* themes
+# **** Create pages
+# **** Create posts
+# **** Install plugins
+
+
+#ADVANCED FUNCTIONALITY
+# **** install boilerplate ACF
+# **** add backWPup jobs
+# **** Configure the White Label CMS
+
+#WRAPUP
+# **** run backWPup job (if created)
+# **** backup database (if above fails)
+
+#SMALLER TASKS
+# **** change URLs
